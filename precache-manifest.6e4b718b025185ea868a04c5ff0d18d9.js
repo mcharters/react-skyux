9d17b229b8b5d9355aea9750524a4aadd153ee30
Matthew Charters <matthew@thirdsectorpublishing.ca>
2021-06-15 14:18:16 -0400

    Updates

---
+++ precache-manifest.6e4b718b025185ea868a04c5ff0d18d9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "479b74beee5c15d620ee8e04bf83ece4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "507cc6ffc6bdd6f1a4d8",
+    "url": "./static/css/2.b809b00f.chunk.css"
+  },
+  {
+    "revision": "e9f95a9198f861ee245f",
+    "url": "./static/css/main.dfce0bd1.chunk.css"
+  },
+  {
+    "revision": "507cc6ffc6bdd6f1a4d8",
+    "url": "./static/js/2.3dd2ddff.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.3dd2ddff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9f95a9198f861ee245f",
+    "url": "./static/js/main.b444669a.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.b444669a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6b0fe98f6cd1c238fc4",
+    "url": "./static/js/runtime-main.790cae8d.js"
+  }
+]);
